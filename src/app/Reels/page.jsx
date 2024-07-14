9bb5caf968d302f043d65deb16ac252c2b0fd82b
Pranaam19 <22j34.pranam@sjec.ac.in>
2024-07-14 20:46:21 +0530

    Created different routes foe a basic social media site

---
+++ src/app/Reels/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Reels = () => {
+  return (
+    <div>
+        <header>
+            <h1 className='flex justify-center text-4xl font-serif m-5'>Reels</h1>
+        </header>
+        <main>
+            domain
+        </main>
+    </div>
+  )
+}
+
+export default Reels
---
+++ src/app/account/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const acoount = () => {
+  return (
+    <div>
+        <header>
+            <h1>Profile</h1>
+        </header>
+        <div>
+            <h3>Account</h3>
+            <p>Username: </p>
+            <p>Email: </p>
+            <p>Password: </p>
+        </div>
+    </div>
+  )
+}
+
+export default acoount
